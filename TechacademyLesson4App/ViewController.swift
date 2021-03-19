872d31ba97a7995e4015fa32d958a00d2c4c469e
koux2 <koux2.g@gmail.com>
2021-03-19 22:52:11 +0900

    Initial Commit

---
+++ TechacademyLesson4App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechacademyLesson4App
+//
+//  Created by koux2 on 2021/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
